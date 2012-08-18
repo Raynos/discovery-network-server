2211c6a7a06900213cf7474842bb53495372369b
Raynos <raynos2@gmail.com>
2012-08-17 23:21:02 -0700

    behave better

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var http = require("http")
     , shoe = require("mux-demux-shoe")
     , DiscoveryNetwork = require("discovery-network")
 
-var server = http.createServer().listen(8081)
+var server = http.createServer(function (req, res) {
+        res.end("discoverynetwork is service. Use it in the uri")
+    }).listen(8081)
     , network = DiscoveryNetwork()
 
-shoe(network).install(server, "/shoe")
+shoe(network).install(server, "/service")
 
 console.log("running on port 8081")
